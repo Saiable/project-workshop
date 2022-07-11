41c9c03c9f956b0fedd6bd648859e349c648411f
sai <2495620791@qq.com>
2022-07-11 20:15:51 +0800

    2022/07/11 周一 20:15:50.90

--- auto-push.bat
+++ auto-push.bat
@@ -1,19 +1,16 @@
 @echo off
 echo "DOCS PUSH BAT"
 
-echo "1. Move to working directory"
 
-cd D:\workspace\github\code\project-workshop
-
-echo "2. Start submitting code to the local repository"
+echo "1. Start submitting code to the local repository"
 git add .
  
-echo "3. Commit the changes to the local repository"
+echo "2. Commit the changes to the local repository"
 set now=%date% %time%
 echo "Time:" %now%
 git commit -m "%now%"
  
-echo "4. Push the changes to the remote git server"
+echo "3. Push the changes to the remote git server"
 git push
  
 echo "Batch execution complete!"
