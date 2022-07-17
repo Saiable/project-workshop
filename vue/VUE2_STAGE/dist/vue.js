7440bd6389221cde8e5e78c5fbf724e14f06d035
sai <2495620791@qq.com>
2022-07-18 07:18:04 +0800

    2022/07/18 周一  7:18:03.93

--- vue/VUE2_STAGE/dist/vue.js
+++ vue/VUE2_STAGE/dist/vue.js
@@ -202,6 +202,33 @@
       var vm = this;
       vm.$options = options;
       initState(vm);
+
+      if (options.el) {
+        vm.$mount(options.el);
+      }
+    };
+
+    Vue.prototype.$mount = function (el) {
+      var vm = this;
+      el = document.querySelector(el);
+      var ops = vm.options;
+
+      if (!ops.render) {
+        // 先查找render函数
+        var template;
+
+        if (!ops.template && el) {
+          // 没有template配置项，但是有el配置项
+          template = el.outerHTML; // 就用el的配置项，outHTML返回的是匹配到自身的dom元素
+        } else {
+          // 如果有template,就用template配置项
+          if (el) {
+            template = ops.template;
+          }
+        }
+
+        console.log(template);
+      }
     };
   }
 
