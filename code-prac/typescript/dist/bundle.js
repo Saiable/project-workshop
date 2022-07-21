f1ef1b5ddda7e78806159671ea41d4df0bb783a7
sai <2495620791@qq.com>
2022-07-22 07:24:17 +0800

    2022/07/22 周五  7:24:16.90

--- code-prac/typescript/dist/bundle.js
+++ code-prac/typescript/dist/bundle.js
@@ -1,10 +1,7 @@
 (function () {
-	'use strict';
+    'use strict';
 
-	let max = Number.MAX_SAFE_INTEGER;
-	console.log(max + 1 === max + 2); // true 取到js中的最大值，然后相加，结果为true，很明显是有问题的
-	BigInt(max);
-	console.log(BigInt(max) + BigInt(1) === BigInt(max) + BigInt(2)); // false
+
 
 })();
 //# sourceMappingURL=bundle.js.map
--- code-prac/typescript/src/index.ts
+++ code-prac/typescript/src/index.ts
@@ -1,4 +1,5 @@
-let max = Number.MAX_SAFE_INTEGER
-console.log(max + 1 === max + 2); // true 取到js中的最大值，然后相加，结果为true，很明显是有问题的
-let r1:bigint = BigInt(max)
-console.log(BigInt(max) + BigInt(1) === BigInt(max) + BigInt(2)) // false
+type IFn = (x: number, y: number) => number
+let res: IFn = (x, y) => {
+    return x + y
+}
+res('a', 2)
