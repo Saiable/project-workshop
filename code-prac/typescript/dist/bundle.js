6ad775ef2132b4be5bccfd81176f0803404a9cf2
sai <2495620791@qq.com>
2022-07-23 07:35:48 +0800

    2022/07/23 周六  7:35:48.35

--- code-prac/typescript/dist/bundle.js
+++ code-prac/typescript/dist/bundle.js
@@ -1,7 +1,17 @@
 (function () {
     'use strict';
 
-
+    class Pointer {
+        constructor(x, y) {
+            // ts比较特殊，在使用必须先声明类型
+            this.x = 1;
+            this.y = 2;
+            // this.x = x
+            // this.y = y
+        }
+    }
+    const res = new Pointer(100, 200);
+    console.log(res, res.x);
 
 })();
 //# sourceMappingURL=bundle.js.map
--- code-prac/typescript/src/index.ts
+++ code-prac/typescript/src/index.ts
@@ -1,5 +1,13 @@
-type IFn = (x: number, y: number) => number
-let res: IFn = (x, y) => {
-    return x + y
+class Pointer {
+    // ts比较特殊，在使用必须先声明类型
+    x: number = 1
+    y: number = 2
+
+    constructor(x: number, y: number) {
+        // this.x = x
+        // this.y = y
+    }
 }
-res('a', 2)
+
+const res = new Pointer(100, 200)
+console.log(res, res.x)
