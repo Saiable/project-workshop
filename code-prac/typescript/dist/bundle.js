837b282f5c59584b8c2663d5bac3d8584a030627
sai <2495620791@qq.com>
2022-07-25 05:57:27 +0800

    2022/07/25 周一  5:57:26.66

--- code-prac/typescript/dist/bundle.js
+++ code-prac/typescript/dist/bundle.js
@@ -1,17 +1,48 @@
 (function () {
     'use strict';
 
-    class Pointer {
-        constructor(x, y) {
-            // ts比较特殊，在使用必须先声明类型
-            this.x = 1;
-            this.y = 2;
-            // this.x = x
-            // this.y = y
-        }
+    var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    function toUpperCase(target, key) {
+        var val = '';
+        Object.defineProperty(target, key, {
+            get: function () {
+                return val.toUpperCase();
+            },
+            set: function (newVal) {
+                val = newVal;
+            }
+        });
     }
-    const res = new Pointer(100, 200);
-    console.log(res, res.x);
+    function double(num) {
+        return function (target, key) {
+            var val = target[key];
+            Object.defineProperty(target, key, {
+                get: function () {
+                    return val * 2;
+                }
+            });
+        };
+    }
+    var Person = /** @class */ (function () {
+        function Person() {
+            this.name = 'sai';
+        }
+        Person.age = 18;
+        __decorate([
+            toUpperCase
+        ], Person.prototype, "name", void 0);
+        __decorate([
+            double()
+        ], Person, "age", void 0);
+        return Person;
+    }());
+    var p = new Person();
+    console.log(p.name, Person.age);
 
 })();
 //# sourceMappingURL=bundle.js.map
--- code-prac/typescript/src/index.ts
+++ code-prac/typescript/src/index.ts
@@ -1,13 +1,32 @@
-class Pointer {
-    // ts比较特殊，在使用必须先声明类型
-    x: number = 1
-    y: number = 2
-
-    constructor(x: number, y: number) {
-        // this.x = x
-        // this.y = y
-    }
+function toUpperCase(target: any, key: string) { // target指的是原型
+    let val: string = ''
+    Object.defineProperty(target, key, {
+        get() {
+            return val.toUpperCase()
+        },
+        set(newVal: string) { //给原型对象上，赋值的时候，就会触发set
+            val = newVal
+        }
+    })
 }
 
-const res = new Pointer(100, 200)
-console.log(res, res.x)
+function double(num:number) {
+    return function (target:any, key:string) { // target指向类
+        let val:number = target[key]
+        Object.defineProperty(target, key,  {
+            get() {
+                return val * 2
+            }
+        })
+    }
+}
+class Person {
+    @toUpperCase
+    public name: string = 'sai'
+
+    @double(2)
+    static age:number = 18
+}
+
+let p = new Person()
+console.log(p.name, Person.age)
