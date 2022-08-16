05af07eb37aea649d74eb3b6050cbd3bbbd9dead
sai <2495620791@qq.com>
2022-08-17 07:02:59 +0800

    2022/08/17 周三  7:02:59.12

--- code-prac/英雄联盟官网首页/js/index.js
+++ code-prac/英雄联盟官网首页/js/index.js
@@ -1,2 +1,18 @@
-import './head_box.js' // 不是webpack环境，后缀要加上
-import './swiper_container.js'
+import './head_box.js';
+
+//编写一些公共的逻辑
+let commMoreArrowList = Array.from(document.querySelectorAll('.comm-more-arrow'))
+commMoreArrowList.forEach(item => {
+    let parent = item.parentNode
+    if(!parent) return
+    parent.addEventListener('mouseenter', function () {
+        item.className = 'comm-more-arrow move'
+        // item.classList.add('move');
+    })
+    parent.addEventListener('mouseleave', function () {
+        item.className = 'comm-more-arrow'
+        // item.classList.remove('move');
+    })
+})
+
+
