c3609c0c98c4d1dcd0feaf98fcb6b894ae4e4e4f
sai <2495620791@qq.com>
2022-10-18 22:16:38 +0800

    2022/10/18 周二 22:16:37.73

--- code-prac/小程序/网易云音乐/study/musicStudy/pages/index/index.js
+++ code-prac/小程序/网易云音乐/study/musicStudy/pages/index/index.js
@@ -4,6 +4,7 @@ Page({
     data: {
         bannerList: [], // 轮播图数据
         recommendList: [], // 推荐歌单数据
+        topList: [], // 排行榜数据
     },
     onLoad: async function(options) {
         console.log('index加载了')
@@ -19,5 +20,30 @@ Page({
         this.setData({
             recommendList: recommendListData.result
         })
+
+        // 获取排行榜数据
+        /**
+         * 需求分析：
+         *  1.需要根据idx的值获取对应的数据
+         *  2.idx的取值范围是0-20，我们需要0-4
+         *  3.需要发5次请求
+         */
+        let index = 0,
+            resultArr = []
+        while(index < 5) {
+            let topListData = await request('/top/list', {idx: index++})
+            let topListItem  = {
+                name: topListData.playlist.name,
+                tracks: topListData.playlist.tracks.slice(0, 3)
+            }
+            resultArr.push(topListItem)
+            // 更新topList的值，发一次就更新一次（多渲染几次，用户体验较好，弱网用户不会有较长时间的白屏）
+            this.setData({
+                topList: resultArr
+            })
+        }
+
+
+
     }
 })
