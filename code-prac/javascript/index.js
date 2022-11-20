19957b3aa58f1c5952304a8ec247ba4124567c4b
sai <2495620791@qq.com>
2022-11-20 12:04:26 +0800

    2022/11/20 周日 12:04:25.77

--- code-prac/javascript/index.js
+++ code-prac/javascript/index.js
@@ -1,15 +1,5 @@
-function sum() {
-    console.log(arguments)
-    let total = null
-    for (let i = 0; i <arguments.length; i++) {
-        let item = Number(arguments[i])
-        if(isNaN(item)) {
-            continue
-        }
-        total += item;
-    }
-    return total
-}
-
-let total = sum(1, 3, '5a')
-console.log(total)
+let arr = [12, 34, 56, 78]
+arr.splice(arr.length, 0, 'AA')
+console.log(arr) // [12, 34, 56, 78, 'AA']
+arr.splice(0, 0, 'BB')
+console.log(arr) // ['BB', 12, 34, 56, 78, 'AA']
